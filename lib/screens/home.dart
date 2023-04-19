bc7311317f8f8ed94db3b0ba5c10da53164d4779
Prince2347X <prince.raj29042004@gmail.com>
2023-04-19 15:48:17 +0530

    Fixed persistency of login

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 
 import 'package:todo/screens/signin.dart';
@@ -37,29 +38,29 @@ class _HomeScreenState extends State<HomeScreen> {
                   },
                   child: authProvider.isAuthenticated
                       ? const TodosScreen()
-                      : AnimatedSwitcher(
-                          duration: const Duration(
-                            seconds: 1,
-                          ),
-                          child: StreamBuilder(
-                            builder: ((context, snapshot) {
-                              if (snapshot.connectionState == ConnectionState.waiting) {
-                                return const Center(
-                                  key: Key('loading'),
-                                  child: CircularProgressIndicator(),
-                                );
-                              } else if (snapshot.hasError) {
-                                return const Center(
-                                  key: Key('error'),
-                                  child: Text('Something went wrong.'),
-                                );
-                              } else {
-                                return const SignInScreen(
-                                  key: Key('signin'),
-                                );
-                              }
-                            }),
-                          ),
+                      : StreamBuilder(
+                        stream: FirebaseAuth.instance.authStateChanges(),
+                          builder: ((context, snapshot) {
+                            if (snapshot.connectionState == ConnectionState.waiting) {
+                              return const Center(
+                                key: Key('loading'),
+                                child: CircularProgressIndicator(),
+                              );
+                            } else if (snapshot.hasError) {
+                              return const Center(
+                                key: Key('error'),
+                                child: Text('Something went wrong.'),
+                              );
+                            } else if (snapshot.hasData) {
+                              return const TodosScreen(
+                                key: Key('todos'),
+                              );
+                            } else {
+                              return const SignInScreen(
+                                key: Key('signin'),
+                              );
+                            }
+                          }),
                         ),
                 ),
                 if (authProvider.isSigningIn)
