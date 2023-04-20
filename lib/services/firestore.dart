387fb6fb5a502ca35a00e95b53b29b3d3afc2a20
Prince2347X <prince.raj29042004@gmail.com>
2023-04-20 10:38:50 +0530

    Added FireStore Services for CRUD operations

---
+++ lib/services/firestore.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:todo/objects/todo.dart';
+
+class FirestoreServices {
+  static FirestoreServices? _instance;
+  late final FirebaseFirestore _firestore;
+  late final CollectionReference _todosCollection;
+
+  FirestoreServices._internal() {
+    _firestore = FirebaseFirestore.instance;
+    _todosCollection = _firestore.collection('todos');
+  }
+
+  static FirestoreServices get instance {
+    _instance ??= FirestoreServices._internal();
+    return _instance!;
+  }
+
+  void createTodo(TodoObject todo) async {
+    final newDocRef = _todosCollection.doc();
+    final newDocData = todo.toJson();
+    newDocData['id'] = newDocRef.id;
+    await newDocRef.set(newDocData);
+  }
+
+  void updateTodo(TodoObject todo) async {
+    Map<String, dynamic> updatedData = todo.toJson();
+    updatedData['modifiedAt'] = DateTime.now().toIso8601String();
+    await _todosCollection.doc(todo.id).update(updatedData);
+  }
+
+  void deleteTodo(TodoObject todo) async {
+    await _todosCollection.doc(todo.id).delete();
+  }
+}
