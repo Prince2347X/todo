659ebee9dae94aceb99d145237364e183e3cacec
Prince2347X <prince.raj29042004@gmail.com>
2023-04-21 08:53:00 +0530

    Removed `Consumer` widget to improve performance

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,61 +17,57 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<GoogleSignInProvider>(
-      builder: (BuildContext context, authProvider, Widget? child) {
-        return SafeArea(
-          child: Scaffold(
-            body: Stack(
-              children: [
-                AnimatedSwitcher(
-                  duration: const Duration(
-                    milliseconds: 400,
-                  ),
-                  transitionBuilder: (Widget widget, Animation<double> animation) {
-                    return SlideTransition(
-                      position: Tween<Offset>(
-                        begin: const Offset(0.0, 0.9),
-                        end: Offset.zero,
-                      ).animate(animation),
-                      child: widget,
-                    );
-                  },
-                  child: authProvider.isAuthenticated
-                      ? const TodosScreen()
-                      : StreamBuilder(
-                        stream: FirebaseAuth.instance.authStateChanges(),
-                          builder: ((context, snapshot) {
-                            if (snapshot.connectionState == ConnectionState.waiting) {
-                              return const Center(
-                                key: Key('loading'),
-                                child: CircularProgressIndicator(),
-                              );
-                            } else if (snapshot.hasError) {
-                              return const Center(
-                                key: Key('error'),
-                                child: Text('Something went wrong.'),
-                              );
-                            } else if (snapshot.hasData) {
-                              return const TodosScreen(
-                                key: Key('todos'),
-                              );
-                            } else {
-                              return const SignInScreen(
-                                key: Key('signin'),
-                              );
-                            }
-                          }),
-                        ),
-                ),
-                if (authProvider.isSigningIn)
-                  const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-              ],
-            ),
-          ),
-        );
-      },
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            AnimatedSwitcher(
+              duration: const Duration(
+                milliseconds: 400,
+              ),
+              transitionBuilder: (Widget widget, Animation<double> animation) {
+                return SlideTransition(
+                  position: Tween<Offset>(
+                    begin: const Offset(0.0, 0.9),
+                    end: Offset.zero,
+                  ).animate(animation),
+                  child: widget,
+                );
+              },
+              child: Provider.of<GoogleSignInProvider>(context, listen: false).isAuthenticated
+                    ? const TodosScreen()
+                    : StreamBuilder(
+                      stream: FirebaseAuth.instance.authStateChanges(),
+                        builder: ((context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.waiting) {
+                            return const Center(
+                              key: Key('loading'),
+                              child: CircularProgressIndicator(),
+                            );
+                          } else if (snapshot.hasError) {
+                            return const Center(
+                              key: Key('error'),
+                              child: Text('Something went wrong.'),
+                            );
+                          } else if (snapshot.hasData) {
+                            return const TodosScreen(
+                              key: Key('todos'),
+                            );
+                          } else {
+                            return const SignInScreen(
+                              key: Key('signin'),
+                            );
+                          }
+                        }),
+                      ),
+              ),
+            if (Provider.of<GoogleSignInProvider>(context, listen: false).isSigningIn)
+              const Center(
+                child: CircularProgressIndicator(),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
